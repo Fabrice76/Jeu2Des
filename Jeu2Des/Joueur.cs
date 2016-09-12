f8641e2115c6aa4cac01e575a5cc0772e9d050e6
FV <fabrice.vajente@2isa.org>
2016-09-12 14:21:03 +0200

    DEBUT DU PROJET JEU2DES

---
+++ Jeu2Des/Joueur.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu2Des
+{
+    public class Joueur
+    {
+
+        #region "Propriétés et accesseurs"
+
+        private string _Nom;
+
+        public string Nom
+        {
+            get { return _Nom; }
+            set { _Nom = value; }
+        }
+
+        #endregion
+        #region "Constructeurs"
+        #endregion
+        #region "Methodes"
+        #endregion
+        #region "Methodes héritées et substituées"
+        #endregion
+        #region "Methodes à implementer pour les interfaces"
+        #endregion
+
+
+    }
+}
